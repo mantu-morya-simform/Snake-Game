0f809081aac6c4dec99093414dc33301e01fa00a
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-11 16:40:41 +0530

    fix: Made Suggested Changes

--- src/Utility/drawFoodSnake.ts
+++ src/Utility/drawFoodSnake.ts
@@ -2,6 +2,16 @@ import { cells, grid, snake } from "../Constraint/gameData";
 import { randomFood } from "./getRandomFood";
 
 export function drawFoodSnake() {
+  //add  class to odd and even for diff style
+  cells.forEach((cell, index) => {
+    cell.className = "cell";
+    if (index % 2 === 0) {
+      cell.classList.add("even");
+    } else {
+      cell.classList.add("odd");
+    }
+  });
+
   //draw snake
   snake.forEach((part) => {
     const snakePartIndex = part.y * grid + part.x;
