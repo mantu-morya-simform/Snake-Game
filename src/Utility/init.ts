73028627e83cbdd5ce5691f72b9ce4b27b836812
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-11 18:51:42 +0530

    fix: conflict issue

--- src/Utility/init.ts
+++ src/Utility/init.ts
@@ -6,7 +6,6 @@ export function init() {
   for (let i = 0; i < grid; i++) {
     for (let j = 0; j < grid; j++) {
       let element: HTMLParagraphElement = document.createElement("p");
-      if (!element) return;
       element.classList.add("cell");
       mainApp.appendChild(element);
       cells.push(element);
