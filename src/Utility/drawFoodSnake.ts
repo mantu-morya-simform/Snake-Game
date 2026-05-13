877d14c963ec5b3a7466c331a0373c2a64fe6a37
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-13 11:53:27 +0530

    fix: suggested PR changes

--- src/Utility/drawFoodSnake.ts
+++ src/Utility/drawFoodSnake.ts
@@ -2,18 +2,33 @@ import { cells, food, grid, snake } from "../Constraint/gameData";
 import appleImg from "../assets/apple.png";
 
 export function drawFoodSnake() {
+  /**
+   * @description make cells innerHtml Blank Initially
+   */
   cells.forEach((cell) => {
     cell.className = "cell";
-    cell.innerHTML = "";
+    // Remove previous image children that i added
+    while (cell.firstChild) {
+      cell.removeChild(cell.firstChild);
+    }
   });
 
+  /**
+   * @description get all snake part and add class and behalf of the class add style for snake
+   */
   snake.forEach((part) => {
     const snakePartIndex = part.y * grid + part.x;
     cells[snakePartIndex].classList.add("snake");
   });
 
+  /**
+   * @description create a food at random location on grid and also add class "food"
+   */
   const foodIndex = food.y * grid + food.x;
   cells[foodIndex].classList.add("food");
-  cells[foodIndex].innerHTML =
-    `<img class="food" src="${appleImg}" alt="apple_png" />`;
+  const img = document.createElement("img");
+  img.src = appleImg;
+  img.alt = "apple_png";
+  img.className = "food";
+  cells[foodIndex].appendChild(img);
 }
